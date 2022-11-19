f2d86591e2e10178fd199b99a06c453fd9ccf387
Ilya Grigorev <facekn110388@gmail.com>
2022-11-19 11:36:19 +0300

    Найти сумму элементов от M до N

---
+++ Sem9_Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int AdditionNum(int a, int b)
+{
+    if (a == b) return b;
+    return a + AdditionNum(a + 1, b);
+};
+
+Console.Write("Введите первое число диапазона: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите последнее число диапазона: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Сумма диапазона равна: ");
+
+if (m > n) Console.WriteLine(AdditionNum(n, m));
+else if (n > m) Console.WriteLine(AdditionNum(m, n));
