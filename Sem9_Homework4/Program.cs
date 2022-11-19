40cede0b38fc87abc66db0f1e085720c8e2adae9
Ilya Grigorev <facekn110388@gmail.com>
2022-11-19 12:07:47 +0300

    Задача №4

--- Sem9_Homework4/Program.cs
+++ Sem9_Homework4/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Написать программу показывающие первые N чисел, для которых каждое следующее 
 // равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
 
- void SumNumb(int a, int b, int n, int m)
+void SumNumb(int a, int b, int n, int m)
 {
     if (n <= m)
     {
         int sum = a + b;
         Console.Write($"{sum} ");
-        SumNumb(b,sum,n+1,m);
-        
+        SumNumb(b, sum, n + 1, m);
+
     }
 }
 
@@ -18,4 +18,4 @@ Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите кол-во вывода: ");
 int c = int.Parse(Console.ReadLine() ?? "0");
-SumNumb(a, b, 1, c); 
+SumNumb(a, b, 1, c);
