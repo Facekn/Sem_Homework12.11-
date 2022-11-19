2e46663b75a9703ba8d4d85da799a531ef03e764
Ilya Grigorev <facekn110388@gmail.com>
2022-11-19 11:13:15 +0300

    Показать натуральные числа от M до N

---
+++ Sem9_Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PosNumbers(int a, int b)
+{
+    if (a <= b)
+    {
+        Console.Write(a + " ");
+        PosNumbers(a + 1, b);
+    }
+};
+void NegNumbers(int a, int b)
+{
+    if (a >= b)
+    {
+        Console.Write(a + " ");
+        NegNumbers(a - 1, b);
+    }
+};
+
+Console.Write("Введите первое число диапазона: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число диапазона: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (m > n) NegNumbers(m, n);
+else if (n > m) PosNumbers(m, n);
